55d6845fc3323e18997d6d03ba474b69c45b92e8
makovey <twomvlad@mail.ru>
2020-05-22 23:07:17 +0300

    [/] Заменены статичные данные, данными из компонента

--- src/app/services/data-handler.service.ts
+++ src/app/services/data-handler.service.ts
@@ -1,9 +1,19 @@
 import {Injectable} from '@angular/core';
+import {Category} from '../model/Category';
+import {TestData} from '../data/TestData';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataHandlerService {
 
-  constructor() { }
+  constructor() {
+  }
+
+  /**
+   * Метод получает массив данных из TestData
+   */
+  getCategories(): Category[] {
+    return TestData.categories;
+  }
 }
--- src/app/view/categories/categories.component.ts
+++ src/app/view/categories/categories.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {DataHandlerService} from '../../services/data-handler.service';
+import {Category} from '../../model/Category';
 
 @Component({
   selector: 'app-categories',
@@ -7,9 +9,21 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[];
 
+  /**
+   * Тестовые данные из TestData передается в сервис
+   * Сервис (injectable) передает данные компоненту
+   * @param dataHandler - экземпляр сервиса
+   */
+  constructor(private dataHandler: DataHandlerService) {
+  }
+
+  /**
+   * Метод отрабатывает после инициализации комппонента
+   */
   ngOnInit(): void {
+    this.categories = this.dataHandler.getCategories();
   }
 
 }
